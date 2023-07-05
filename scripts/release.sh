c7a7fee82e4ba81d05681b680673f464ad29a29d
James harrington <jamesdonaldharrington@gmail.com>
2023-07-05 03:11:49 -0500

    fix: changing the order of release steps

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,13 +3,16 @@
 # Get the current Git branch
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-# Run standard-version
-yarn standard-version --commit false
+# Run standard-version in dry-run mode to get the new version
+output=$(yarn standard-version --dry-run)
 
-# Get the new version number
-version=$(node -p -e "require('./package.json').version")
+# Parse the new version from the output
+version=$(echo "$output" | grep -o 'new version.*' | awk '{print $3}')
 
 # Create and switch to new release branch
 git checkout -b "release/$version"
 
-echo "Switched to new branch 'release/$version'"
+# Run the actual standard-version to update files and make a commit
+yarn standard-version
+
+echo "Switched to new branch 'release/$version' and made a release commit"
