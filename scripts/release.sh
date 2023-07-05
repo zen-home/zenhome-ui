bd1b2656f466c57e758c4bcd8e3b01b51091bc69
James harrington <jamesdonaldharrington@gmail.com>
2023-07-05 03:07:59 -0500

    fix: skip commit and switch to release branch

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,11 +3,13 @@
 # Get the current Git branch
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-# Check if the current branch starts with "release/"
-if [[ $current_branch != release/* ]]; then
-    echo "Releases can only be made from a release branch. Please switch to a release branch and try again."
-    exit 1
-fi
-
 # Run standard-version
-yarn standard-version
+yarn standard-version --commit false
+
+# Get the new version number
+version=$(node -p -e "require('./package.json').version")
+
+# Create and switch to new release branch
+git checkout -b "release/$version"
+
+echo "Switched to new branch 'release/$version'"
