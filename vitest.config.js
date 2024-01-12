ca42b24929f95ab6595d5056951f447a0f8ee04f
James harrington <jamesdonaldharrington@gmail.com>
2024-01-12 00:27:23 -0600

    build: use node 20.x

--- vitest.config.js
+++ vitest.config.js
@@ -6,6 +6,10 @@ import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    // enable hydration mismatch details in production build
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true
+  },
   server: {
     https: {
       key: fs.readFileSync('./key.pem'),
